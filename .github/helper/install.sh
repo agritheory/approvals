6cc008118480c5146d56b0b42cd25842dca5f0b7
Tyler Matteson <tyler@agritheory.com>
2022-12-14 14:21:39 -0500

    test: iterating on CI

--- .github/helper/install.sh
+++ .github/helper/install.sh
@@ -15,6 +15,11 @@ cd ~ || exit
 
 pip install --upgrade pip
 pip install frappe-bench
+git clone https://github.com/frappe/frappe --branch version-14 --depth 1
+bench init --skip-assets --frappe-path ~/frappe --python "$(which python)" frappe-bench
+
+mkdir ~/frappe-bench/sites/test_site
+cp -r "${GITHUB_WORKSPACE}/.github/helper/site_config.json" ~/frappe-bench/sites/test_site
 
 mysql --host 127.0.0.1 --port 3306 -u root -e "SET GLOBAL character_set_server = 'utf8mb4'"
 mysql --host 127.0.0.1 --port 3306 -u root -e "SET GLOBAL collation_server = 'utf8mb4_unicode_ci'"

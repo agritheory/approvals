5427215d243c43ba2f8df87d6d1de0365e590619
Rohan Bansal <rohan@agritheory.dev>
2024-06-21 17:36:52 +0530

    fix: handle case where fallback approver is not set

--- approvals/approvals/api.py
+++ approvals/approvals/api.py
@@ -1,7 +1,9 @@
 import json
 
 import frappe
+from frappe import _
 from frappe.desk.form.utils import add_comment
+from frappe.model.workflow import get_workflow_name
 
 
 @frappe.whitelist()
@@ -29,6 +31,10 @@ def get_approval_roles(doc, method=None):
 
 	if not roles:
 		fallback_approver = settings.fallback_approver_role
+		if not fallback_approver:
+			frappe.throw(
+				_("No approvers found. Please set a fallback approver role in Document Approval Settings.")
+			)
 		roles.append(fallback_approver)
 	return roles
 
@@ -83,7 +89,11 @@ def fetch_approvals_and_roles(doc, method=None):
 			}
 		)
 		add_roles.append(_role)
-	return add_roles
+	approval_state = (
+		frappe.get_value("Workflow", get_workflow_name(doc.doctype), "custom_approval_state")
+		or "Pending"
+	)
+	return {"approvals": add_roles, "approval_state": approval_state}
 
 
 @frappe.whitelist()
@@ -105,8 +115,12 @@ def approve_document(doc, method=None, role=None, user=None):
 	checked_all = check_all_document_approvals(doc, method, include_role=role, user=user)
 	if checked_all:
 		doc = frappe.get_doc(doc.doctype, doc.name)
-		doc.submit()
-		doc.set_status(update=True, status="Approved")
+		if doc.meta.is_submittable:
+			doc.submit()
+			doc.set_status(update=True, status="Approved")
+		else:
+			doc.save(ignore_permissions=True)
+			doc.set_status(update=True, status="Approved")
 
 	return approval
 
@@ -137,8 +151,8 @@ def set_status_to_approved(doc, method=None, automatic=False):
 def reject_document(doc, role=None, comment="", method=None):
 	doc = frappe._dict(json.loads(doc)) if isinstance(doc, str) else doc
 	doc = frappe.get_doc(doc.doctype, doc.name)
-	doc.status = "Rejected"
-	doc.save()
+	doc.save(ignore_permissions=True)
+	doc.set_status(update=True, status="Rejected")
 	rejection = add_comment(doc.doctype, doc.name, comment, frappe.session.user, frappe.session.user)
 	revoke_approvals_on_reject(doc, method)
 	return rejection
@@ -182,7 +196,7 @@ def add_user_approval(doc, method=None, user=None):
 	uda.reference_doctype = doc.doctype
 	uda.reference_name = doc.name
 	uda.approver = user
-	uda.save()
+	uda.save(ignore_permissions=True)
 	frappe.db.commit()
 
 
@@ -201,7 +215,7 @@ def remove_user_approval(doc, method=None, user=None):
 	removal.comment_email = frappe.session.user
 	removal.content = f"<b>{user}<b> removed as approver by <b>{frappe.session.user}</b>"
 	removal.subject = "Approver removed"
-	removal.save()
+	removal.save(ignore_permissions=True)
 	return
 
 
@@ -217,4 +231,12 @@ def create_approval_notification(doc, user):
 	no.document_name = doc.name
 	no.from_user = doc.owner
 	no.email_content = f"{doc.doctype} {doc.name} requires your approval"
-	no.save()
+	try:
+		no.save(ignore_permissions=True)
+	except AttributeError:
+		# missing outgoing email account error
+		frappe.msgprint(
+			_(
+				"Approval notification delivery failed. Please setup a default Email Account from Setup > Email > Email Account"
+			),
+		)

796f0a55e93ad5d50aa24f830b8fb0e34ecb3e3d
Rohan <Alchez@users.noreply.github.com>
2024-07-01 23:15:05 +0530

    feat: format vue files to composition API (v14) (#23)
    * feat: use vite as builder for approvals
    
    * ci: update pytest runner
    
    * fix: update approvals file type
    
    * fix: handle errors
    
    * ci: update runner dependencies
    
    * fix: include css, reverse show/hide of status
    
    ---------
    
    Co-authored-by: Rohan Bansal <rohan@agritheory.dev>
    Co-authored-by: Tyler Matteson <tyler@agritheory.com>

--- approvals/approvals/doctype/document_approval/document_approval.py
+++ approvals/approvals/doctype/document_approval/document_approval.py
@@ -1,6 +1,6 @@
 import frappe
+from frappe import _
 from frappe.model.document import Document
-from frappe.utils.data import today
 
 
 class DocumentApproval(Document):
--- approvals/public/js/approvals.bundle.js
+++
@@ -1,1 +0,0 @@
-import './approvals/approvals.js'

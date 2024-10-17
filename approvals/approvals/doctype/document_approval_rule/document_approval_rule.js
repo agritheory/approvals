8b30643ace782e64b49703445ae0052ff578019e
Francisco Roldán <franciscoproldan@gmail.com>
2024-10-17 11:12:37 -0300

    Validate Python in condition field Document Approval Rule  (#69)
    * feat: validate condition field
    
    * feat: test condition
    
    * feat: dry option to apply
    
    * fix: error messages

--- approvals/approvals/doctype/document_approval_rule/document_approval_rule.js
+++ approvals/approvals/doctype/document_approval_rule/document_approval_rule.js
@@ -1,4 +1,37 @@
 frappe.ui.form.on('Document Approval Rule', {
-	// refresh: function(frm) {
-	// }
+	refresh: function (frm) {
+		frm.add_custom_button(__('Test Condition'), function () {
+			let d = new frappe.ui.Dialog({
+				title: __('Select: {0}', [frm.doc.approval_doctype]),
+				fields: [
+					{
+						label: __(frm.doc.approval_doctype),
+						fieldname: 'docname',
+						fieldtype: 'Link',
+						options: frm.doc.approval_doctype,
+						reqd: 1,
+					},
+				],
+				primary_action_label: __('Test'),
+				primary_action(values) {
+					frappe.call({
+						method: 'run_doc_method',
+						args: {
+							method: 'test_condition',
+							dt: frm.doc.doctype,
+							dn: frm.doc.name,
+							args: { doctype: frm.doc.approval_doctype, docname: values.docname },
+						},
+						callback: function (r) {
+							if (r.message) {
+								frappe.msgprint(r.message)
+							}
+						},
+					})
+					d.hide()
+				},
+			})
+			d.show()
+		})
+	},
 })
